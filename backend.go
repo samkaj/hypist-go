7cb44ebf90fa85df163276ac5ed028873f82a881
samkaj <kajavasamuel@gmail.com>
2023-04-25 21:54:19 +0200

    Fix: clean up main and make db accessible in pkg
    Signed-off-by: samkaj <kajavasamuel@gmail.com>

--- backend.go
+++ backend.go
@@ -8,15 +8,24 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+var dbClient *mongo.Client
+
 func main() {
+  setupDb()
+  startServer()
+}
+
+func setupDb() {
 	opts := options.Client().ApplyURI("mongodb://localhost:27017")
 	var err error
-	client, err := mongo.Connect(context.Background(), opts)
+	dbClient, err := mongo.Connect(context.Background(), opts)
 	if err != nil {
 		panic(err)
 	}
-	defer client.Disconnect(context.Background())
+	defer dbClient.Disconnect(context.Background())
+}
 
+func startServer() {
 	r := gin.Default()
 	r.POST("/users", PostUser)
 	r.Run()
