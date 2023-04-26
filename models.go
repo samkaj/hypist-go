813b09244421f763338e8e775e9490c6683381b6
samkaj <kajavasamuel@gmail.com>
2023-04-26 20:23:33 +0200

    Fix: fix missed encoding stuff
    Signed-off-by: samkaj <kajavasamuel@gmail.com>

--- models.go
+++ models.go
@@ -16,7 +16,7 @@ type User struct {
 
 type Run struct {
 	Gamemode       Gamemode `bson:"gameMode" json:"gameMode"`
-	TimeMs         int64    `bson:"timeMs "json:"timeMs"`
+	TimeMs         int64    `bson:"timeMs" json:"timeMs"`
 	CorrectWords   int8     `bson:"correctWords" json:"correctWords"`
 	IncorrectWords int8     `bson:"incorrectWords" json:"incorrectWords"`
 	Seed           int64    `bson:"seed" json:"seed"`
